b235d6bde3dfbd7655fd1096668968751eaebce4
7044622686 <101453765+7044622686@users.noreply.github.com>
2022-11-25 21:14:22 +0530

    Update app.js
    try-and-try

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,17 +1,18 @@
-            Enter First Number :
+            Enter First Number : <br>
             <input type="text" id="Text1" name="TextBox1">
-            
+            <br>
 
-            Enter Second Number :
+            Enter Second Number : <br>
             <input type="text" id="Text2" name="TextBox2">
-            
+            <br>
 
-            Result :
+            Result : <br>
             <input type="text" id="txtresult" name="TextBox3">
-            
+            <br>
 
          <input type="button" name="clickbtn" value="Display Result" onclick="add_number()">
 
+        <script type="text/javascript">
 
         function add_number() {
             var first_number = parseInt(document.getElementById("Text1").value);
@@ -19,3 +20,5 @@
             var result = first_number + second_number;
             document.getElementById("txtresult").value = result;
         }
+
+        </script>
