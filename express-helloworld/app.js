3bdc155fa06d8790931c37030918b37e9da2997f
7044622686 <101453765+7044622686@users.noreply.github.com>
2022-11-25 18:59:21 +0530

    Update app.js
    Last to last try

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,9 +1,26 @@
-// store input numbers
-const num1 = parseInt(prompt('Enter the first number '));
-const num2 = parseInt(prompt('Enter the second number '));
+<body>
+    <div>
+        <div>
+            <h1>Add two number using text box as input using javascript</h1>
+        </div>
+            Enter First Number : <br>
+            <input type="text" id="Text1" name="TextBox1">
+            <br>
+            Enter Second Number : <br>
+            <input type="text" id="Text2" name="TextBox2">
+            <br>
+            Result : <br>
+            <input type="text" id="txtresult" name="TextBox3">
+            <br>
+         <input type="button" name="clickbtn" value="Display Result" onclick="add_number()">
 
-//add two numbers
-const sum = num1 + num2;
+        <script type="text/javascript">
+        function add_number() {
+            var first_number = parseInt(document.getElementById("Text1").value);
+            var second_number = parseInt(document.getElementById("Text2").value);
+            var result = first_number + second_number;
+            document.getElementById("txtresult").value = result;
+        }
+        </script>
 
-// display the sum
-console.log(`The sum of ${num1} and ${num2} is ${sum}`);
+
