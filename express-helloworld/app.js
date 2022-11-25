c45c694eb3186e7f95ea219ffb4f2b99a5447e8b
7044622686 <101453765+7044622686@users.noreply.github.com>
2022-11-25 15:57:38 +0530

    Update app.js
    Update to JS Addition

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,20 +1,29 @@
-html>  
-<body>  
-<form method="post">  
-Enter First Number:  
-<input type="number" name="number1" /><br><br>  
-Enter Second Number:  
-<input type="number" name="number2" /><br><br>  
-<input  type="submit" name="submit" value="Add">  
-</form>  
-<?php  
-    if(isset($_POST['submit']))  
-    {  
-        $number1 = $_POST['number1'];  
-        $number2 = $_POST['number2'];  
-        $sum =  $number1+$number2;     
-echo "The sum of $number1 and $number2 is: ".$sum;   
-}  
-?>  
-</body>  
+<html>
+<body>
+    <div>
+        First Number : <br>
+        <input type="text" id="text1">
+        <br>
+        Second Number : <br>
+        <input type="text" id="text2">
+        <br>
+        Result : <br>
+        <input type="text" id="res">
+        <br>
+        <input type="button" value="Display Result" onclick="addNumber()">
+    </div>
+
+        <script>
+
+            function addNumber(){
+                var f_num = parseInt(document.getElementById("text1").value);
+                var s_num = parseInt(document.getElementById("text2").value);
+
+                var sum = f_num + s_num;
+
+                document.getElementById("res").value = sum;  
+
+            }
+        </script>
+    </body>
 </html>
